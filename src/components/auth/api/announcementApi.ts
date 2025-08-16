0506333ae71fddf3c4ff581630e041edb0b436fd
Uday Chauhan <chauhanuday842@gmail.com>
2025-08-17 01:18:18 +0530

    fixes auth cors 17/08/2025 01:18am

--- src/components/auth/api/announcementApi.ts
+++ src/components/auth/api/announcementApi.ts
@@ -12,6 +12,15 @@ const apiClient = axios.create({
 });
 
 
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  console.log("token", token)
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+})
+
 
 export interface ApiResponse<T> {
     success: boolean;
